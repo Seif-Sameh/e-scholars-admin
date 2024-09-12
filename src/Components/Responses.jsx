c93b11a4f507323e2b351bb908fb4f8a59eb0bd4
Seif Sameh <seifsameh2626@gmail.com>
2024-09-13 02:50:33 +0300

    initial commit

--- src/Components/Responses.jsx
+++ src/Components/Responses.jsx
@@ -41,8 +41,7 @@ const Responses = ({ grade, section }) => {
                                 <tr >
                                     <th className='text-center w-[60px] p-0 text-xl border-r-[6px]  border-white max-md:text-base text-white rounded-lg'> <div className='bg-[#054bb4] py-2 w-full h-full rounded-tl-lg'>No.</div></th>
                                     <th className='text-start  p-0 text-xl max-md:text-base  text-white rounded-md'> <div className='bg-[#054bb4] py-2 pl-4 w-full h-full '>Name</div></th>
-                                    <th className='text-center p-0 text-xl max-md:text-base  text-white rounded-md'> <div className='bg-[#054bb4] py-2 w-full h-full '>Marks</div></th>
-                                    <th className='text-center p-0 text-xl max-md:text-base text-white rounded-md'><div className='bg-[#054bb4] py-2 w-full  rounded-tr-md'>Submittion time</div></th>
+                                    <th className='text-center p-0 text-xl max-md:text-base  text-white rounded-md'> <div className='bg-[#054bb4] py-2 w-full h-full rounded-tr-lg'>Marks</div></th>
                                 </tr>
                             </thead>
                             <tbody >
@@ -51,9 +50,11 @@ const Responses = ({ grade, section }) => {
                                         <>
                                             <tr key={index} className={`relative rounded-lg ${index % 2 == 0 ? 'bg-white' : 'bg-gray-200'}`}>
                                                 <td className='text-center border-y-[6px] border-r-[6px] border-white bg-[#054bb4] rounded-l-lg text-white font-bold  py-2 text-lg max-md:text-base'>{index + 1}</td>
-                                                <td className='text-start border-y-[6px] border-white pl-4 py-2 text-lg max-md:text-base '>{item[0]}</td>
-                                                <td className='text-center border-y-[6px] border-white px-2 py-2 text-lg max-md:text-base'>{item[2]}</td>
-                                                <td className='text-center border-y-[6px] border-white rounded-r-lg px-2 text-lg max-md:text-base '>{item[1].slice(0, 16)}</td>
+                                                <td className='text-start border-y-[6px] border-white pl-4 py-2 text-lg max-md:text-base '>
+                                                    <p>{item[0]}</p>
+                                                    <p className='text-sm text-slate-600'>submitted {item[1].slice(0, 10)} at {item[1].slice(11, 16)}</p>
+                                                </td>
+                                                <td className='text-center border-y-[6px] border-white px-2 py-2 text-lg max-md:text-base rounded-r-lg'>{item[2]}</td>
                                             </tr>
                                         </>
                                     ))
@@ -65,7 +66,6 @@ const Responses = ({ grade, section }) => {
                                             <tr key={index} className={`relative rounded-lg ${index % 2 == 0 ? 'bg-white' : 'bg-gray-200'}`}>
                                                 <td className='text-center border-y-[6px] border-r-[6px] border-white bg-[#054bb4] rounded-l-lg text-white font-bold  py-2 text-lg max-md:text-base'>{attendance.length + index + 1}</td>
                                                 <td className='text-start border-y-[6px] border-white pl-4 py-2 text-lg max-md:text-base '>{item}</td>
-                                                <td className='text-center border-y-[6px] border-white px-2 py-2 text-lg max-md:text-base'>-</td>
                                                 <td className='text-center border-y-[6px] border-white rounded-r-lg px-2 text-lg max-md:text-base '>-</td>
                                             </tr>
                                         </>
